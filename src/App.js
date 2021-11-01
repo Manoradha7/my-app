23e61d09fae417bd32a5725ee3c317d45bdad6b6
Manoradha7 <manoradha7@gmail.com>
2021-11-01 20:15:07 +0530

    sample

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+function Msg({name},{id}) {
+  return(
+    <div>
+      <img className="img-pic" src="https://www.seekpng.com/png/detail/506-5061704_cool-profile-avatar-picture-cool-picture-for-profile.png" alt="profile-pic" />
+      <h1> Hello {name} {id}</h1>
     </div>
-  );
+  )
 }
 
-export default App;
+export default function App(){
+  console.log("HI");
+  const names=["Mano","Saha","Viru","Mega","Mukesh"];
+  return(
+    <div className="App">
+      {/* <Msg name="Mano" id="01"/>
+      <Msg name="Saha" id="02"/>
+      <Msg name="Viru" id="03"/> */}
+      {names.map(n => <Msg name={n} />)}
+    </div>
+  )
+}
